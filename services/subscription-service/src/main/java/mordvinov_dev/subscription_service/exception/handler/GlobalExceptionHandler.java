f43434a0122ddb04c219d4541da61a196e2efb2d
Ilya Mordvinov <morilya111@gmail.com>
2026-03-03 10:40:08 +0300

    feat: реализовать pom.xml для api-gateway

--- services/subscription-service/src/main/java/mordvinov_dev/subscription_service/exception/handler/GlobalExceptionHandler.java
+++ services/subscription-service/src/main/java/mordvinov_dev/subscription_service/exception/handler/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import mordvinov_dev.subscription_service.exception.handler.dto.ValidationErrorR
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
